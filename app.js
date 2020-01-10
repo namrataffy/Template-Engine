82e1d7b42124a487e2aec422e05f164dd4796c14
Arman Riahi <namrataffy@gmail.com>
2020-01-10 14:03:51 -0800

    generating html elements basic done

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/employee");
+const Intern = require("./lib/intern");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
 
 const q1 = [
   {
     name: "role",
     message: "What is the role of this employee?",
     type: "list",
-    choices: ["manager", "engineer", "intern"]
+    choices: ["manager", "engineer", "intern", "exit"]
   }
 ];
 const qIntern = [
@@ -27,6 +32,11 @@ const qIntern = [
   {
     name: "school",
     message: "What is the School of this employee?",
+    type: "input"
+  },
+  {
+    name: "role",
+    message: "Confirm the role of this employee",
     type: "input"
   }
 ];
@@ -51,6 +61,11 @@ const qEng = [
     name: "github",
     message: "What is the Github of this employee?",
     type: "input"
+  },
+  {
+    name: "role",
+    message: "Confirm the role of this employee",
+    type: "input"
   }
 ];
 
@@ -72,24 +87,129 @@ const qManager = [
   },
   {
     name: "officeID",
-    message: "What is the OfficeID of this employee?",
+    message: "What is the Office number of this employee?",
+    type: "input"
+  },
+  {
+    name: "role",
+    message: "Confirm the role of this employee",
     type: "input"
   }
 ];
 
-inquirer.prompt(q1).then(answers => {
-  console.log(answers);
+let interns = [];
+let engineers = [];
+let managers = [];
+let people = [];
+
+function theWork(answers) {
   if (answers.role == "intern") {
-    inquirer.prompt(qIntern).then(answers => {
-      console.log(answers);
-    });
+    inquirer
+      .prompt(qIntern)
+      .then(answers => {
+        let intern1 = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school,
+          answers.role,
+          answers.school
+        );
+        interns.push(intern1);
+        people.push(intern1);
+
+        console.log(interns);
+      })
+      .then(() => {
+        inquirer.prompt(q1).then(theWork);
+      });
   } else if (answers.role == "engineer") {
-    inquirer.prompt(qEng).then(answers => {
-      console.log(answers);
-    });
+    inquirer
+      .prompt(qEng)
+      .then(answers => {
+        let eng1 = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github,
+          answers.role,
+          answers.github
+        );
+        engineers.push(eng1);
+        people.push(eng1);
+
+        console.log(engineers);
+      })
+      .then(() => {
+        inquirer.prompt(q1).then(theWork);
+      });
   } else if (answers.role == "manager") {
-    inquirer.prompt(qManager).then(answers => {
-      console.log(answers);
-    });
+    inquirer
+      .prompt(qManager)
+      .then(answers => {
+        let manager1 = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeID,
+          answers.role,
+          answers.officeID
+        );
+        managers.push(manager1);
+        people.push(manager1);
+
+        console.log(managers);
+      })
+      .then(() => {
+        inquirer.prompt(q1).then(theWork);
+      });
+  } else if (answers.role == "exit") {
+    makeHtml();
+    return;
   }
-});
+}
+
+inquirer.prompt(q1).then(theWork);
+
+function makeHtml() {
+  let page = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+  <style>
+  .cardSmall{
+    width:33%;
+    float: left;
+  }
+  </style>
+  <h1> Team</h1>`;
+  people.forEach(people => {
+    let peopleinfo = `<div class="card  cardSmall">
+    <div class="card-header text-white bg-primary ">
+      <h3>${people.name}</h3>
+      <h3> ${people.title}</h3>
+
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item font-weight-bold">ID: ${people.id}</li>
+            <li class="list-group-item font-weight-bold">Email: <a href="${people.email}">${people.email}</a></li>
+            <li class="list-group-item font-weight-bold">${people.specific}</li>
+
+        </ul>
+    </div>
+    </div>`;
+    page += peopleinfo;
+  });
+  page += `</body></html>`;
+
+  fs.writeFile("newfile.html", page, function(err) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+}
